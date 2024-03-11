1ce2f0af34004912116ff6242ab01196fb812221
Shaquille Mandy <shaqmandy@gmail.com>
2024-03-11 02:25:54 -0300

    fixed fk constraint bug in deleteUser, needed to delete user's cart before deleting the user

--- src/services/user/deleteUser.ts
+++ src/services/user/deleteUser.ts
@@ -2,7 +2,9 @@ import prisma from '..'
 
 // Deletes A User In The Database
 const deleteUser = async (userId: string) => {
-  // delete and return the user
+  // delete all the user's cart items and cart before deleting the user
+  await prisma.cartItem.deleteMany({ where: { cart: { userId } } })
+  await prisma.cart.delete({ where: { userId } })
   return await prisma.user.delete({ where: { id: userId } })
 }
 
