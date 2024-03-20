ec9e339b506d4f2f6a8a62d67ebc53c36e439ee7
Shaquille Mandy <shaqmandy@gmail.com>
2024-03-20 01:29:10 -0300

    added new dev dependency for render deployment

--- src/utils/secrets.ts
+++ src/utils/secrets.ts
@@ -2,4 +2,3 @@ export const JWT_SECRET = process.env.JWT! // the ! tells typescript that this w
 export const PORT = process.env.PORT
 export const RESEND_API_KEY = process.env.RESEND_API_KEY
 export const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY
-export const FRONTEND_BASE_URL = process.env.FRONTEND_BASE_URL
